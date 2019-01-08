5dee54a73a94b23071a3d273088f18856a0b61b7
gabrielacaesar <gabrielacaesar55@gmail.com>
2019-01-08 00:33:31 -0200

    Update media.py

--- media.py
+++ media.py
@@ -1,17 +1,15 @@
 # importing library
 import webbrowser
 
-#the content below is related to entertainment.py
-#both has the films' content
+# the content below is related to entertainment.py
+# both has the films' content
 
-#fuction to get all the films' content
-#after, the fresh_tomatoes.py will read it 
-#in a html page, where the infos will be displayed
+# fuction to get all the films' content
+# after, the fresh_tomatoes.py will read it
+# in a html page, where the infos will be displayed
 
 class Movie():
-	def __init__(self, movie_title, original_title, 
-				movie_length, movie_year,
-				poster_image, trailer_youtube):
+	def __init__(self, movie_title, original_title, movie_length, movie_year, poster_image, trailer_youtube):
 		self.title = movie_title
 		self.original_title = original_title
 		self.length = movie_length
@@ -19,6 +17,6 @@ class Movie():
 		self.poster_image_url = poster_image
 		self.trailer_youtube_url = trailer_youtube
 
-#function to open the url trailer
+# function to open the url trailer
 	def show_trailer(self):
 		webbrowser.open(self.trailer_youtube_url)
