efeef555af0380d7e834796fa519d5a8a73e1043
gabrielacaesar <gabrielacaesar55@gmail.com>
2019-01-08 00:34:32 -0200

    Update media.py

--- media.py
+++ media.py
@@ -9,14 +9,14 @@ import webbrowser
 # in a html page, where the infos will be displayed
 
 class Movie():
-	def __init__(self, movie_title, original_title, movie_length, movie_year, poster_image, trailer_youtube):
-		self.title = movie_title
-		self.original_title = original_title
-		self.length = movie_length
-		self.year = movie_year
-		self.poster_image_url = poster_image
-		self.trailer_youtube_url = trailer_youtube
+def __init__(self, movie_title, original_title, movie_length, movie_year, poster_image, trailer_youtube):
+	self.title = movie_title
+	self.original_title = original_title
+	self.length = movie_length
+	self.year = movie_year
+	self.poster_image_url = poster_image
+	self.trailer_youtube_url = trailer_youtube
 
 # function to open the url trailer
-	def show_trailer(self):
-		webbrowser.open(self.trailer_youtube_url)
+def show_trailer(self):
+	webbrowser.open(self.trailer_youtube_url)
